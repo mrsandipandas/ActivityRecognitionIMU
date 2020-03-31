3d55c5e0553ceb4a45b0772c81ee78497b18da31
Pedro Roque <padr@kth.se>
2020-04-01 00:46:42 +0200

    Pushing LDA code and filter template.

---
+++ procRawData.m
@@ -0,0 +1,32 @@
+filename = 'walking';
+ld_file = [filename,'.mat'];
+load(filename)
+proc_data = [];
+
+for i=2:size(meas_struct,1)
+   
+   % there are no NANs
+   if isnan(meas_struct(i,:))*ones(11,1) == 0
+       % Get time delta
+       t_before = meas_struct(i-1,1);
+       t_curr = meas_struct(i,1);
+       dt = t_curr - t_before;
+       
+       % Get lin. velocity 
+       accel_before = meas_struct(i-1,6:8);
+       accel_curr = meas_struct(i,6:8);
+       lin_vel = (accel_curr - accel_before)/dt;
+       
+       % Get current data 
+       q = meas_struct(i,2:5);
+       gyro = meas_struct(i,9:11);
+       
+       % Build proc data
+       proc_data_vec = [q, lin_vel, gyro, accel_curr];
+       proc_data = [proc_data; proc_data_vec];
+   end
+end
+label = cell(size(proc_data,1),1);
+label(:) = {filename};
+
+save([filename,'_proc.mat'],'proc_data','label');
